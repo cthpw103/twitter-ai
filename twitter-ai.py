da1bf69c5df7d7b88c02b7b499df50c4f239b2bf
prometheus :) <44480063+prometheus1337@users.noreply.github.com>
2019-03-16 09:11:39 +0000

    fix linting errors and comment code
    moe salaryman

--- twitter-ai.py
+++ twitter-ai.py
@@ -1,19 +1,25 @@
 import sys
 import textgenrnn
 import twitter
+#asks for api stuff
 conkey = input("consumer key?")
 consec = input("consumer secret?")
 accesstok = input ("access token?")
 accesstoksec = input("access token secret?")
 manifest = twitter.api(conkey, consec, accesstok, accesstoksec)
-tl = manifest.getUserTimeline(screen_name=sn, count=cnt)
+# get tl
 cnt = sys.argv[1]
 sn = sys.argv[2]
+tl = manifest.getUserTimeline(screen_name=sn, count=cnt)
+# count and screen names are arguments
 tl = get_tweets(api=manifest, screen_name=sn)
 reo = open('tweetraw.txt', 'w')
 reo.write(tl)
 rnn = textgenrnn()
+# train from tweets
 rnn.train_from_file('tweetraw.txt')
-geno = textgen.generate(3, temperature=1.0)
+#generate with 3 layers
+geno = rnn.generate(3, temperature=1.0)
+# post ai
 final = manifest.PostUpdate(geno)
 print(final.text)
