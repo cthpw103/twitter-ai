4552d05c8ca2385d6e576eef334e009e173341bc
cth103 <cthdev@email.com>
2019-10-22 23:38:20 +0100

    fix ugly + make layers changable

--- twitter-ai.py
+++ twitter-ai.py
@@ -1,25 +1,32 @@
 import sys
 import textgenrnn
 import twitter
+
 #asks for api stuff
-conkey = input("consumer key?")
-consec = input("consumer secret?")
-accesstok = input ("access token?")
-accesstoksec = input("access token secret?")
+conkey = input("Consumer key? ")
+consec = input("Consumer secret? ")
+accesstok = input("Access token? ")
+accesstoksec = input("Access token secret? ")
 manifest = twitter.api(conkey, consec, accesstok, accesstoksec)
+
 # get tl
 cnt = sys.argv[1]
 sn = sys.argv[2]
 tl = manifest.getUserTimeline(screen_name=sn, count=cnt)
+
 # count and screen names are arguments
 tl = get_tweets(api=manifest, screen_name=sn)
 reo = open('tweetraw.txt', 'w')
 reo.write(tl)
 rnn = textgenrnn()
+
 # train from tweets
 rnn.train_from_file('tweetraw.txt')
+
 #generate with 3 layers
-geno = rnn.generate(3, temperature=1.0)
+layers = input("How many layers? ")
+geno = rnn.generate(layers, temperature=1.0)
+
 # post ai
 final = manifest.PostUpdate(geno)
 print(final.text)
